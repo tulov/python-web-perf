50c63a95bb07fb9185693f87beabb17293efb29f
Alexey Tulovskiy <tulov.alex@gmail.com>
2020-06-23 12:22:52 +0000

    replaced test data generators in gens directory

--- gens/csv_test_data.py
+++ gens/csv_test_data.py
@@ -1,24 +1,16 @@
 #!/usr/bin/env python3
-import asyncio
-from aiopg.sa import create_engine
+import csv
+import sys
 import random
 import string
 
 # static seed to generate the same data all the time
 rng = random.Random(0)
 
+csv_writer = csv.writer(sys.stdout)
+csv_writer.writerow(["a", "b"])
 
-async def go():
-    async with create_engine(user='postgres',
-                             database='test',
-                             host='127.0.0.1',
-                             password='postgres') as engine:
-        async with engine.acquire() as conn:
-            await conn.execute('DELETE FROM test')
-            for i in range(1_000_000):
-                short_random_string = "".join(
-                    rng.choice(string.ascii_letters) for _ in range(20))
-                await conn.execute(f"INSERT INTO test VALUES ({i}, '{short_random_string}')")
-
-
-asyncio.run(go())
+for i in range(1_000_000):
+    short_random_string = "".join(
+        rng.choice(string.ascii_letters) for _ in range(20))
+    csv_writer.writerow([i, short_random_string])
--- gens/fill_db_test_data.py
+++ gens/fill_db_test_data.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
-import csv
-import sys
+import asyncio
+import aiopg
 import random
 import string
 
 # static seed to generate the same data all the time
 rng = random.Random(0)
 
-csv_writer = csv.writer(sys.stdout)
-csv_writer.writerow(["a", "b"])
 
-for i in range(1_000_000):
-    short_random_string = "".join(
-        rng.choice(string.ascii_letters) for _ in range(20))
-    csv_writer.writerow([i, short_random_string])
+dsn = 'dbname=test user=postgres password=postgres host=127.0.0.1'
+
+
+async def go():
+    async with aiopg.create_pool(dsn) as pool:
+        async with pool.acquire() as conn:
+            for i in range(1_000_000):
+                short_random_string = "".join(
+                    rng.choice(string.ascii_letters) for _ in range(20))
+                await conn.execute(f"INSERT INTO test VALUES ({i}, '{short_random_string}')")
+
+
+asyncio.run(go())
